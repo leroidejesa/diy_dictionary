ad178306ac04f711623d9d7e7b34da93e523af40
leroi <student@epicodus.com>
2015-05-01 11:47:08 -0700

    Add and pass specs for Definition#id and Definition#find

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,11 +1,12 @@
 class Definition
-  attr_reader(:word_class, :plural_form, :actual_definition)
+  attr_reader(:word_class, :plural_form, :actual_definition, :id)
   @@definitions = []
 
   define_method(:initialize) do |attributes|
     @word_class = attributes.fetch(:word_class)
     @plural_form = attributes.fetch(:plural_form)
     @actual_definition = attributes.fetch(:actual_definition)
+    @id = @@definitions.length().+(1)
   end
 
   define_method(:save) do
@@ -18,6 +19,16 @@ class Definition
 
   define_singleton_method(:clear) do
     @@definitions = []
+  end
+
+  define_singleton_method(:find) do |id|
+    found_definition = nil
+    @@definitions.each() do |definition|
+      if definition.id().eql?(id.to_i())
+        found_definition = definition
+      end
+    end
+    found_definition
   end
 
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -30,6 +30,22 @@ describe(Definition) do
     end
   end
 
+  describe("#id") do
+    it("returns the id of the definition") do
+      test_definition = Definition.new({ :word_class => "noun", :plural_form => "Cacti", :actual_definition => "a succulent plant with typically sharp scales or spines" })
+      test_definition.save()
+      expect(test_definition.id()).to(eq(1))
+    end
+  end
 
+  describe(".find") do
+    it("returns the definition by its ID number") do
+      test_definition = Definition.new({ :word_class => "noun", :plural_form => "Cacti", :actual_definition => "a succulent plant with typically sharp scales or spines" })
+      test_definition.save()
+      test_definition2 = Definition.new({ :word_class => "noun", :plural_form => "Canyons", :actual_definition => "a deep gorge, typically with a river flowing through it" })
+      test_definition2.save()
+      expect(Definition.find(test_definition2.id())).to(eq(test_definition2))
+    end
+  end
 
 end
